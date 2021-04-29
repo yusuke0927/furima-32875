cc5ede2dd58b30410446f9d8d76eb96347cb9563
yusuke0927 <gorirando0927@gmail.com>
2021-04-29 18:08:21 +0900

    商品出品機能

---
+++ app/models/furima.rb
@@ -0,0 +1,14 @@
+class Furima < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :day
+  belongs_to :delivery_charge
+  belongs_to :status
+
+  has_one_attached :image
+
+  validates :furima_name, :description, presence: true
+
+  validates :area_id, :category_id, :day_id, :delivery_charge_id, :status_id, numericality: { other_than: 1 }
+end
