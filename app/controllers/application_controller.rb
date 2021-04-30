55edc67abceab4c97b64208fd3f2a0781e37c9e1
yusuke0927 <gorirando0927@gmail.com>
2021-04-30 21:31:23 +0900

    修正2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+ before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :name_chinese_character_last_name, :name_chinese_character_first_name, :name_catakana_last_name, :name_catakana_first_name, :birthday, :email])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :furimas
   has_many :purchases
+
+  # belongs_to :nick_name
+  # belongs_to :name_chinese_character_last_name
+  # belongs_to :name_chinese_character_first_name
+  # belongs_to :name_catakana_last_name
+  # belongs_to :name_catakana_first_name
+  # belongs_to :birthday
+  # belongs_to :email
+  # belongs_to :encrypted_password
 end
