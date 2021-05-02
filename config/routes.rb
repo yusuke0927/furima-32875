9aea4d96971cf497bc009a0eff739e50de0df571
yusuke0927 <gorirando0927@gmail.com>
2021-05-02 20:20:25 +0900

    修正7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'furimas#index'
-  resources :furimas, only: [:index, :new, :create]
+  #resources :furimas, only: [:index, :new, :create]
 end
--- db/migrate/20210427060912_devise_create_users.rb
+++ db/migrate/20210427060912_devise_create_users.rb
@@ -10,7 +10,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :name_catakana_last_name,              null: false
       t.string :name_catakana_first_name,             null: false
       t.date   :birthday,                             null: false
-      t.string :email,                                null: false, unique: true, default: ""
       t.string :encrypted_password,                   null: false, default: ""
 
       ## Recoverable
