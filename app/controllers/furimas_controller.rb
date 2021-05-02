eb2b22ae781a3efe2590717d3ca19428814cab35
yusuke0927 <gorirando0927@gmail.com>
2021-05-02 21:06:22 +0900

    修正1

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,19 +1,19 @@
-#class FurimasController < ApplicationController
- #  before_action :authenticate_user!, only: [:new]
- # def index
-  #  @furimas = Furima.all
-  #end
+class FurimasController < ApplicationController
+   before_action :authenticate_user!, only: [:new]
+  def index
+    @furimas = Furima.all
+  end
 
-  #def new
-    #@furima = Furima.new
-  #end
+  def new
+    @furima = Furima.new
+  end
 
-  #def create
-    #Furima.create(furima_params)
-  #end
+  def create
+    Furima.create(furima_params)
+  end
 
-  #private
-  #def furima_params
-    #params.require(:furima).permit(:furima_name, :description, :category, :status, :delivery_charge, :area, :day, :price)
-  #end
-#end
+  private
+  def furima_params
+    params.require(:furima).permit(:furima_name, :description, :category, :status, :delivery_charge, :area, :day, :price)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+   Include default devise modules. Others available are:
+   :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  #has_many :furimas
-  #has_many :purchases
+  has_many :furimas
+  has_many :purchases
 
 
   validates :nick_name,                             presence: true
