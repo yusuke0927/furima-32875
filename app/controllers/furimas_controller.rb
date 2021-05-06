7ad44021a9a85cdd2b98bcfeca91820c81ec584b
yusuke0927 <gorirando0927@gmail.com>
2021-05-06 15:44:16 +0900

    商品一覧表示機能

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,8 +1,17 @@
+<<<<<<< Updated upstream
 #class FurimasController < ApplicationController
  #  before_action :authenticate_user!, only: [:new]
  # def index
   #  @furimas = Furima.all
   #end
+=======
+class FurimasController < ApplicationController
+   before_action :authenticate_user!, only: [:new, :create]
+  def index
+    # @furimas = Furima.all
+    @furimas = Furima.includes(:user).order("created_at DESC")
+  end
+>>>>>>> Stashed changes
 
   #def new
     #@furima = Furima.new
@@ -12,8 +21,31 @@
     #Furima.create(furima_params)
   #end
 
+<<<<<<< Updated upstream
   #private
   #def furima_params
     #params.require(:furima).permit(:furima_name, :description, :category, :status, :delivery_charge, :area, :day, :price)
   #end
 #end
+=======
+  def show
+    @furima = Furima.find(params[:id])
+  end
+
+  def edit
+    @furima = Furima.find(params[:id])
+  end
+
+  def destroy
+    @furima = Furima.find(params[:id])
+    @furima.destroy
+    redirect_to root_path
+  end
+
+
+  private
+  def furima_params
+    params.require(:furima).permit(:furima_name, :description, :category_id, :status_id, :delivery_charge_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
+  end
+end
+>>>>>>> Stashed changes
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'furimas#index'
+<<<<<<< Updated upstream
   #resources :furimas, only: [:index, :new, :create]
+=======
+  resources :furimas, only: [:index, :new, :create, :show, :edit, :destroy]
+>>>>>>> Stashed changes
 end
