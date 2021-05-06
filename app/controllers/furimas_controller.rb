aa4a189e04cd30b99e47781d77c60a0911eae0c3
yusuke0927 <gorirando0927@gmail.com>
2021-05-06 15:56:54 +0900

    修正1

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,33 +1,18 @@
-<<<<<<< Updated upstream
-#class FurimasController < ApplicationController
- #  before_action :authenticate_user!, only: [:new]
- # def index
-  #  @furimas = Furima.all
-  #end
-=======
 class FurimasController < ApplicationController
    before_action :authenticate_user!, only: [:new, :create]
   def index
     # @furimas = Furima.all
     @furimas = Furima.includes(:user).order("created_at DESC")
   end
->>>>>>> Stashed changes
 
-  #def new
-    #@furima = Furima.new
-  #end
+  def new
+    @furima = Furima.new
+  end
 
-  #def create
-    #Furima.create(furima_params)
-  #end
+  def create
+    Furima.create(furima_params)
+  end
 
-<<<<<<< Updated upstream
-  #private
-  #def furima_params
-    #params.require(:furima).permit(:furima_name, :description, :category, :status, :delivery_charge, :area, :day, :price)
-  #end
-#end
-=======
   def show
     @furima = Furima.find(params[:id])
   end
@@ -48,4 +33,3 @@ class FurimasController < ApplicationController
     params.require(:furima).permit(:furima_name, :description, :category_id, :status_id, :delivery_charge_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
->>>>>>> Stashed changes
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'furimas#index'
-<<<<<<< Updated upstream
-  #resources :furimas, only: [:index, :new, :create]
-=======
   resources :furimas, only: [:index, :new, :create, :show, :edit, :destroy]
->>>>>>> Stashed changes
 end
